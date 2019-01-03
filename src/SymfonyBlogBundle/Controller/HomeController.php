908869e925921ef42672363a1d03aa3ea7b2a94c
NastySwipy <naskomarinov@yahoo.com>
2019-01-03 19:24:37 +0200

    Prepare for the store part.

--- src/SymfonyBlogBundle/Controller/HomeController.php
+++ src/SymfonyBlogBundle/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace SymfonyBlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use SymfonyBlogBundle\Entity\Article;
+use SymfonyBlogBundle\Entity\Product;
 
 class HomeController extends Controller
 {
@@ -37,13 +38,80 @@ class HomeController extends Controller
      */
     public function storeIndexAction()
     {
-
-        $articles = $this
-            ->getDoctrine()
-            ->getRepository(Article::class)
-            ->findBy([], ['viewCount' => 'desc', 'dateAdded' => 'desc']);
+//        /** @var Product $product */
+//        $product = new Product();
+//        $product->setName('Coca Cola' . rand(1, 1000));
+//        $product->setPrice(2.50);
+//        $product->setDescription('Coca-Cola, or Coke is a carbonated soft drink manufactured by The Coca-Cola Company.
+//        Originally intended as a patent medicine,
+//        it was invented in the late 19th century by John Pemberton and was bought out by businessman Asa Griggs Candler,
+//        whose marketing tactics led Coca-Cola to its dominance of the world soft-drink market throughout the 20th century.
+//        The drink\'s name refers to two of its original ingredients: coca leaves, and kola nuts (a source of caffeine).
+//        The current formula of Coca-Cola remains a trade secret,
+//        although a variety of reported recipes and experimental recreations have been published.');
+//        $product->setCreateDate(new \DateTime('now'));
+//
+//        $em = $this->getDoctrine()->getManager();
+//        $em->persist($product);
+//        $em->flush();
 
         return $this->render('default/storeIndex.html.twig',
-            ['articles' => $articles]);
+            []);
     }
+//
+//    /**
+//     * @Route("/SkyStore/ViewAll")
+//     */
+//    public function viewProducts()
+//    {
+//        /** @var Product $products */
+//        $products = $this
+//            ->getDoctrine()
+//            ->getRepository(Product::class)
+//            ->findAll();
+//        return $this->render('default/storeIndex.html.twig',
+//            ['products' => $products]);
+//
+//    }
+//
+//    /**
+//     * @Route("/SkyStore/edit")
+//     */
+//    public function editProduct()
+//    {
+//        /** @var Product $product */
+//        $product = $this
+//            ->getDoctrine()
+//            ->getRepository(Product::class)
+//            ->find(6);
+//        $product->setName('Pepsi');
+//        $em = $this->getDoctrine()->getManager();
+//        $em->flush();
+//
+//        return $this->redirect('/SkyStore/ViewAll');
+//
+//    }
+//
+//    /**
+//     * @Route("/SkyStore/delete")
+//     */
+//    public function deleteProduct()
+//    {
+//        /** @var Product $product */
+//        $product = $this
+//            ->getDoctrine()
+//            ->getRepository(Product::class)
+//            ->find(6);
+//
+//        if (!$product) {
+//            throw $this->createNotFoundException('Product not found!');
+//        }
+//
+//        $em = $this->getDoctrine()->getManager();
+//        $em->remove($product);
+//        $em->flush();
+//
+//        return $this->redirect('/SkyStore/ViewAll');
+//
+//    }
 }
